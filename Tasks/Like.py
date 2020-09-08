e5bed59e2b3b1ab06ce6c3b15416b5e0a39e9e49
XhionX <X_hion@tut.by>
2020-09-08 16:33:52 +0300

    Like done

--- Tasks/Like.py
+++ Tasks/Like.py
@@ -14,45 +14,66 @@ likes("Alex", "Jacob", "Mark", "Max") -> "Alex, Jacob and 2 others like this"
 Бонусные очки
 Функция работает на нескольких языках и кодировках - язык ответа определяется по языку входного массива.
 '''
-
-
 class MyClass:
 
-    def likes(self, var: str) -> str:
 
-        lst = []
+    def likes(self, var: list) -> str:
+        english = [chr(x) for x in range(ord('a'), ord('z') + 1)]
+        russian = [chr(x) for x in range(ord('а'), ord('а') + 32)]
 
-        result = 'No one like this'
+        def language_definition(var: list) -> str:
+            var1 = ','.join(var).lower()
+            for i in var1:
+                if i in english:
+                    return 'english'
+                elif i in russian:
+                    return 'russian'
+            else:
+                return 'english'
 
-        if len(var) > 0:
+        if isinstance(var, list):
+            pass
+        elif not isinstance(var, list):
+            print('Вы ввели неправильный тип данных ')
+            raise TypeError(' the type of incoming values does not match the expected values')
 
-            var = var.replace(',', '')
-            lst = ' '.join(var.split())
-            lst = lst.split(" ")
-
-            if len(lst) == 1:
-                lst = " ".join(x.strip('"') for x in lst) + ' likes this'
-
-            elif len(lst) == 2:
-                lst = " and ".join(x.strip('"') for x in lst) + ' like this'
-
-            elif len(lst) == 3:
-                lst = f'{lst[0]}, {lst[1]} and {lst[2]} like this'
-                lst = lst.replace('"', '')
-
-            elif len(lst) > 3:
-                lst = f'{lst[0]}, {lst[1]} and {len(lst) - 2} others like this'
-                lst = lst.replace('"', '')
-
-            return lst
+        if len(var) == 0:
+            if language_definition(var) == 'english':
+                result = 'no one likes this.'
+            elif language_definition(var) == 'russian':
+                result = 'никому это не нравится.'
+        if len(var) == 1:
+            if language_definition(var) == 'english':
+                result = var[0] + ' likes this.'
+            elif language_definition(var) == 'russian':
+                result = var[0] + ' отметил, что ему это нравится.'
+        if len(var) == 2:
+            if language_definition(var) == 'english':
+                result = var[0] + ' and ' + var[1] + ' like this.'
+            elif language_definition(var) == 'russian':
+                result = var[0] + ' и ' + var[1] + ' отметили, что им это нравится.'
+        if len(var) == 3:
+            if language_definition(var) == 'english':
+                result = var[0] + ', ' + var[1] + ' and ' + var[2] + ' like this.'
+            elif language_definition(var) == 'russian':
+                result = var[0] + ', ' + var[1] + ' и ' + var[2] + ' отметили, что им это нравится.'
+        if len(var) > 3:
+            others = len(var) - 2
+            if language_definition(var) == 'english':
+                result = var[0] + ', ' + var[1] + ' and ' + str(others) + ' like this.'
+            elif language_definition(var) == 'russian':
+                result = var[0] + ', ' + var[1] + ' и еще ' + str(others) + ' отметили, что им это нравится.'
 
         return result
+
 
 if __name__ == '__main__':
     # Here we can make console input and check how function works
 
-    var = input('Input names: ')
+    var = []
+
 
     result = MyClass().likes(var)
 
     print(result)
+
